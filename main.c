c41261c96e767aa52c105e4d7960e608d8b43109
Levan Jandieri <le.jandieri@gmail.com>
2019-06-05 22:58:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 1024
-#define INITIAL_SIZE 256
+#define BUFFER_SIZE 32
+#define INITIAL_SIZE 32
 
 char * replaceWord(char * replacement)
 {
     int l = strlen(replacement);
     char * newWord;
-    newWord = (char *) malloc(l+1);
+    newWord = (char *) malloc(l);
     strcpy(newWord,replacement);
-
     return newWord;
 }
 
@@ -44,14 +43,18 @@ char * findWord(char * fileName, char * word) //
                 break;
             }
     }
-    if (wordFound) { line += l + 1;}
+    if (wordFound) {
+        line += l + 1; // slide pointer to the position after the ':' in dictionary line
+        line[strlen(line)-1]='\0'; // replace the '\n' character at the end of the line with '\0'
+    }
     else
     {
-        char * result = (char *) malloc(l + 3);
-        result[0] = '{';
-        const char * newword = replaceWord(word);
+        char * result = (char *) malloc(2);
+        result[0] = '<';
+        result[1] = '\0';
+        const char * newword = replaceWord(word); // TO DO: Clear newword after use, so it is not lodged in memory forever
         strcat(result,newword);
-        strcat(result,"}");
+        strcat(result,">");
         return result;
     }
 
@@ -59,19 +62,33 @@ char * findWord(char * fileName, char * word) //
     return line;
 }
 
+int checkChar(char c, char * delims)
+{
+    if (strchr(delims,c)!=NULL) // strchr returns NULL if 'c' is not found in 'delims'
+    {
+        return 1; // char is a letter or number
+    }
+    else
+    {
+        return 0; // char is a seperator
+    }
+}
+
+
 
 int main() {
-    char *input = (char *) malloc(INITIAL_SIZE);
+    char *currentWord = (char *) malloc(INITIAL_SIZE);
+    char * delims1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // non-separators
     char *newBuffer;
     char ch;
     int charCounter = 0, currentBufferSize = INITIAL_SIZE;
     //char * q = "Hello";
     //q = replaceWord("123456789");
     //findWord("demo.wb","Hello");
-    //printf("%s",findWord("Buch.txt","for"));
 
 
-    char *delims = (char *) malloc(256-10-24-24+1);
+
+/*    char *delims = (char *) malloc(256-10-24-24+1);
     int delimCounter = 0;
     for (int i = 1; i<256; i++) //
     {
@@ -87,33 +104,73 @@ int main() {
     char *token;
 
     token = strtok(str, delims);
-
+    printf("%s\n",str);
     while( token != NULL ) {
         printf( " %s\n", token );
 
         token = strtok(NULL, delims);
-    }
+    }*/
 
-    while ((ch = getchar()) != '\n')
+/*    while ((ch = getchar()) != '\n') // get input
     {
-            input[charCounter] = ch;
+            if (!checkChar(ch,delims1))
+            {
+
+            }
+            currentWord[charCounter] = ch;
             charCounter++;
             if (charCounter >= currentBufferSize)
             {
-                newBuffer = realloc(input,sizeof(input)+BUFFER_SIZE); // sizeof(input) doesn't actually work
+                newBuffer = realloc(currentWord,sizeof(currentWord)+BUFFER_SIZE); // sizeof(input) doesn't actually work
                 currentBufferSize+= BUFFER_SIZE;
                 if( newBuffer == NULL) {
                     perror("reallocation of variable input failed.");
                     //handle realloc error
                 }
-                input = newBuffer;
+                currentWord = newBuffer;
             }
+    }*/
+
+
+    while ((ch = getc(stdin)) != '~') // get input
+    {
+        if (!checkChar(ch,delims1))
+        {
+            currentWord[charCounter] = '\0';
+            charCounter = 0;
+
+            if (strlen(currentWord)>0) //if 'currentWord' is not empty
+            {
+                printf(findWord("demo.wb",currentWord));
+                printf("%c",ch);
+                currentBufferSize = INITIAL_SIZE;
+                currentWord = realloc(currentWord,INITIAL_SIZE);
+            }
+            else // if 'currentWord' is empty, then last 'ch' was a separator
+            {
+                printf("%c",ch);
+            }
+        }
+        else
+        {
+            currentWord[charCounter] = ch;
+            charCounter++;
+
+            if (charCounter >= currentBufferSize) {
+                newBuffer = realloc(currentWord, currentBufferSize + BUFFER_SIZE);
+                currentBufferSize += BUFFER_SIZE;
+                if (newBuffer == NULL) {
+                    perror("reallocation for 'currentWord' failed.");
+                    //handle realloc error
+                }
+                currentWord = newBuffer;
+                free(newBuffer);
+            }
+        }
     }
 
-    input[charCounter] = '\0';
+    // currentWord[charCounter] = '\0'; // is this needed?
 
-    printf("%s\n",input);
-    printf("%d",currentBufferSize);
 
     return 0;
 }
